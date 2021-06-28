303a2f1a1414677ab770e52fdbb07e244d571e0c
Leandro Guedes <leandroguedes@protonmail.com>
2021-06-28 14:39:32 -0300

    First setup

---
+++ MathClient.cpp
@@ -0,0 +1,16 @@
+// MathClient.cpp
+#include <iostream>
+#include "MathLibrary.h"
+
+int main(int argc, char const *argv[])
+{
+    double a = 7.4;
+    int b = 99;
+
+    std::cout << "a + b = " << MathLibrary::Arithmetic::Add(a, b) << std::endl;
+    std::cout << "a - b = " << MathLibrary::Arithmetic::Subtract(a, b) << std::endl;
+    std::cout << "a * b = " << MathLibrary::Arithmetic::Multiply(a, b) << std::endl;
+    std::cout << "a / b = " << MathLibrary::Arithmetic::Divide(a, b) << std::endl;
+
+    return 0;
+}
